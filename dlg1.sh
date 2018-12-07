ee55f4e652df55b7b9401a1d1a4c54bfc8948880
Stefan Kombrink <katakombi@gmail.com>
2018-12-07 18:22:55 +0100

    installer dlg1

---
+++ dlg1.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+OWNDIR=`dirname "$(readlink -f "$0")"`
+cd $OWNDIR
+
+./udocker --repo="$PWD/../.dolmades/repo" run --user=$(whoami) --volume=$OWNDIR:/install --hostenv runtime sh -c \
+'yad --width=500 --height=400 \
+ --window-icon=/install/dolmade.png \
+ --image=/install/dolmade.png \
+ --sticky \
+ --on-top \
+ --center \
+ --title "Installation of Dolmades v1.0 - The Goglizer" \
+ --text "This will install Dolmades v1.0 - The Goglizer on this machine" \
+ --wrap --form --field="Do you accept the Apache license?:TXT" \
+ "$(cat /install/LICENSE)" \
+ --button=gtk-no:1 --button=gtk-yes:0'
