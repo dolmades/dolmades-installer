53e396e3f16153fa08ce7de636c4e070fb6ec1d5
Stefan Kombrink <katakombi@gmail.com>
2018-12-07 19:10:42 +0100

    WIP

---
+++ prepare_payload.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+DOLMADES_VERSION="1.0"
+OLDDIR=$PWD
+PAYLOAD=$PWD/payload
+
+function prepare_payload() {
+	mkdir -p $PAYLOAD
+	cd $PAYLOAD
+	curl -L -o dolmades.tgz "https://github.com/dolmades/dolmades-cli/archive/v$DOLMADES_VERSION.tar.gz"
+	tar -xzf dolmades.tgz
+	sed -i.orig 's#HOME = .*$#HOME = "'$PAYLOAD'"#' $PAYLOAD/dolmades-cli-$DOLMADES_VERSION/config.py
+	cd dolmades-cli-$DOLMADES_VERSION/
+	mkdir -p $PAYLOAD/.dolmades
+	./udocker mkrepo $PAYLOAD/.dolmades/repo
+	echo "Pulling images..."
+	START=$(date +%s.%N)
+	./udocker --repo="$PAYLOAD/.dolmades/repo" pull dolmades/runtime:$DOLMADES_VERSION
+	./udocker --repo="$PAYLOAD/.dolmades/repo" pull dolmades/winestable:$DOLMADES_VERSION
+	./udocker --repo="$PAYLOAD/.dolmades/repo" pull dolmades/winestaging:$DOLMADES_VERSION
+	#./udocker --repo="$PAYLOAD/.dolmades/repo" pull dolmades/winedevel:$DOLMADES_VERSION
+	END=$(date +%s.%N)
+	echo "...done after "$(echo "$END - $START" | bc)" seconds"
+	cp $OLDDIR/install.sh $PAYLOAD
+	chmod +x $PAYLOAD/install.sh
+	cp $OLDDIR/dolmade.png $PAYLOAD/dolmades-cli-$DOLMADES_VERSION/dolmade.png
+	cp $OLDDIR/dlg?.sh $PAYLOAD/dolmades-cli-$DOLMADES_VERSION/
+	chmod +x $PAYLOAD/dolmades-cli-$DOLMADES_VERSION/dlg?.sh
+}
+
+rm -rf $PAYLOAD
+prepare_payload
