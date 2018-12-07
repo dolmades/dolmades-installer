8b81d512ced677a8d0f83531764b4f882caa2885
Stefan Kombrink <katakombi@gmail.com>
2018-12-07 18:56:36 +0100

    final dlg

---
+++ dlg5.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+OWNDIR=`dirname "$(readlink -f "$0")"`
+cd $OWNDIR
+
+./udocker --quiet --repo="$PWD/../.dolmades/repo" run --user=$(whoami) --volume=$OWNDIR:/install --hostenv runtime sh -c \
+'yad \
+ --window-icon=/install/dolmade.png \
+ --image=/install/dolmade.png \
+ --sticky \
+ --on-top \
+ --center \
+ --title "Installation of Dolmades v1.0 - The Goglizer" \
+ --text "Installation succeeded!" \
+ --wrap \
+ --button=gtk-ok'
