8443c4734e2121eafb7d45bad67fe7aec31ff01e
Stefan Kombrink <katakombi@gmail.com>
2018-12-08 14:16:07 +0100

    initial commit

---
+++ xdg/install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+xdg-desktop-menu install \
+ dolmades-dolmades.directory \
+ dolmades-dolmades-the_goglizer.desktop \
+ dolmades-dolmades-uninstall.desktop
+
+xdg-icon-resource install --size 64 dolmades-dolmade-64.png dolmades-menu
+xdg-icon-resource install --size 22 dolmades-dolmade-22.png dolmades-menu
+xdg-icon-resource install --size 64 dolmades-remove-64.png dolmades-uninstall
+xdg-icon-resource install --size 22 dolmades-remove-22.png dolmades-uninstall
+
+#xdg-icon-resource install --size 64 dolmades-cook-64.png dolmades-cook
+#xdg-icon-resource install --size 64 dolmades-ingredient-64.png dolmades-ingredient
+#xdg-icon-resource install --size 64 dolmades-recipe-64.png dolmades-recipe
+#xdg-icon-resource install --size 64 dolmades-tools-64.png dolmades-tools
+#xdg-icon-resource install --size 64 dolmades-run-64.png dolmades-run
---
+++ xdg/uninstall.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+xdg-icon-resource uninstall --size 64 dolmades-dolmade-64.png dolmades-menu
+xdg-icon-resource uninstall --size 22 dolmades-dolmade-22.png dolmades-menu
+xdg-icon-resource uninstall --size 64 dolmades-remove-64.png dolmades-uninstall
+xdg-icon-resource uninstall --size 22 dolmades-remove-22.png dolmades-uninstall
+
+xdg-desktop-menu uninstall dolmades-dolmades.directory dolmades-dolmades.directory *.desktop
